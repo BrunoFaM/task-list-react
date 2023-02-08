e33a740fe575e1bc6a7b7e0ae022d842d2760082
Bruno M <brunomoron56@gmail.com>
2023-02-08 16:44:16 -0300

    first commit

---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useState, useEffect } from "react";
+import { task as data } from "../data/task";
+
+export const TaskContext = createContext();
+
+export function TaskContextProvider(props) {
+  const [tasks, setTask] = useState([]);
+
+  function createTask(task) {
+    setTask([
+      ...tasks,
+      {
+        title: task.title,
+        id: tasks.length,
+        description: task.description,
+      },
+    ]);
+  }
+
+  function deleteTask(taskId) {
+    setTask(tasks.filter((task) => task.id !== taskId));
+  }
+
+  useEffect(() => {
+    setTask(data);
+  }, []);
+
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks: tasks,
+        deleteTask: deleteTask,
+        createTask: createTask,
+      }}
+    >
+      {props.children}
+    </TaskContext.Provider>
+  );
+}
---
+++ src/data/task.js
@@ -0,0 +1,17 @@
+export const task = [
+  {
+    id: 0,
+    title: "my first task",
+    description: "fist task",
+  },
+  {
+    id: 1,
+    title: "my second task",
+    description: "second task",
+  },
+  {
+    id: 2,
+    title: "my third task",
+    description: "third task",
+  },
+];
